4a40714b39fdf333c0f7bf82f8bf2ef6669cde75
Angel <zaggel2002@gmail.com>
2022-04-28 23:56:10 -0500

    Rutas anidadas listas

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,5 +1,5 @@
 import { lazy, LazyExoticComponent } from "react";
-//import { LazyPage1, LazyPage2, LazyPage3 } from "../01-lazyload/pages";
+import { NoLazy } from "../01-lazyload/pages/NoLazy"; //lo importo porque este no será mediante Carga Perezosa
 
 type JSXComponent = () => JSX.Element 
 
@@ -10,27 +10,19 @@ interface Route {
     name: string
 }
 
-const Lazy1 = lazy(() => import(/*webpackChunkName: "LazysitoPage1"*/ '../01-lazyload/pages/LazyPage1'))
-const Lazy2 = lazy(() => import(/*webpackChunkName: "LazysitoPage2"*/ '../01-lazyload/pages/LazyPage2'))
-const Lazy3 = lazy(() => import(/*webpackChunkName: "LazysitoPage3"*/ '../01-lazyload/pages/LazyPage3'))
+const LazyLayout = lazy(() => import(/*webpackChunkName: "LazyLayout"*/ '../01-lazyload/layout/LazyLayout'))
 
 export const routes:Route[] = [
     {
-        to: '/lazy1',
-        path: 'lazy1',
-        Component: Lazy1,
-        name: 'Lazi-1'
+        path: '/lazyload/*', //Todo(*) lo que venga despues de la ruta /lazyload, será procesado por este path
+        to: '/lazyload/',
+        Component: LazyLayout,
+        name: 'LaziLayout'
     },
     {
-        to: '/lazy2',
-        path: 'lazy2',
-        Component: Lazy2,
-        name: 'Lazi-2'
-    },
-    {
-        to: '/lazy3',
-        path: 'lazy3',
-        Component: Lazy3,
-        name: 'Lazi-3'
-    },
+        to: '/no-lazy',
+        path: 'no-lazy',
+        Component: NoLazy,
+        name: 'No Lazy'
+    }
 ]
